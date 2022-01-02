b086ed13c1cef4934dda06c7ab9382b51a54b562
Muhammed Rabih <rabihofcl@gmail.com>
2022-01-03 00:17:19 +0530

    session 6 completed

---
+++ base/views/order_views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from base.models import Product
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+# Create your views here.
