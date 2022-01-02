00a8274ca0bebf200dfdcfdf90399b11b594071a
Muhammed Rabih <rabihofcl@gmail.com>
2022-01-02 17:11:28 +0530

    33 completed

--- base/views.py
+++ base/views.py
@@ -6,7 +6,24 @@ from .models import Product
 
 from .serializers import ProductSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Create your views here.
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self,attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
